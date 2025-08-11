6e1e3b402911e883664216f0c24de9a49ebc4582
Onyekwelu "Deuce" Jesse <onyekwelujesse1234@gmail.com>
2025-08-11 18:00:39 +0100

    update: made modifications to payload in vital signs

--- src/services/vitalSIgnsServices.js
+++ src/services/vitalSIgnsServices.js
@@ -9,17 +9,18 @@ export const createVitalSign = async (vitalSignData) => {
     diastolicBloodPressure,    // to be stored in the "blood_pressure_diastolic" column (integer)
     weight,                    // to be stored in the "weight" column (numeric)
     heartRate,                 // to be stored in the "pulse_rate" column (integer)
-    spo2,                      // to be stored in the "spo2" column (integer)
+    spo2,     
+    recordedBy,      // to be stored in the "recorded_by" column (integer)
   } = vitalSignData;
 
   const result = await query(
     `INSERT INTO vital_signs (
        patient_id, recorded_at, temperature, 
        blood_pressure_systolic, blood_pressure_diastolic, 
-       weight, pulse_rate, spo2
-     ) VALUES ($1, $2, $3, $4, $5, $6, $7, $8) 
+       weight, pulse_rate, spo2, recorded_by, created_at
+     ) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, NOW()) 
      RETURNING *;`,
-    [patientId, date, temperature, systolicBloodPressure, diastolicBloodPressure, weight, heartRate, spo2]
+    [patientId, date, temperature, systolicBloodPressure, diastolicBloodPressure, weight, heartRate, spo2, recordedBy]
   );
 
   return result.rows[0];
